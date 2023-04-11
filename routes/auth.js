d5a1ee42f6cc335d3933f244dcf98d19a297a969
Tyler <2465627+BigManatee@users.noreply.github.com>
2023-04-11 15:27:33 +0100

    Adjustments to routing

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const router = express.Router();
-
 const { register, login, logout } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-
-module.exports = router;
+module.exports = function(app){
+    app.post('/register', register);
+    app.post('/login', login);
+    app.post('/logout', logout);
+}
