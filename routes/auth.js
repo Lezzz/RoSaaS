6ba5a13ea4d3a3b496941da1fc29cd6af36b7403
Tyler J <hello@iamtyler.sh>
2023-04-11 16:02:59 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { register, login, logout } = require('../controllers/auth');
 
 module.exports = function(app){
-    app.post('/register', register);
-    app.post('/login', login);
-    app.post('/logout', logout);
+    app.post('/api/auth/register', register);
+    app.post('/api/auth/login', login);
+    app.post('/api/auth/logout', logout);
 }
