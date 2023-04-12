f7fa559307b24ec64c070bff43ede1284b3cb771
Valentin Lazureanu <valentin.lazureanu@gmail.com>
2023-04-12 22:30:32 +0300

    ??

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -2,5 +2,9 @@ const express = require('express');
 const stripeController = require('../controllers/stripe');
 
 module.exports = function(app) {
-    app.post('/api/stripe/webhook', express.raw({ type: 'application/json' }), stripeController.createWebhook);
+    app.post('/api/stripe/webhook', express.raw({ type: 'application/json' }), (req, res, next) => {
+        console.log('Webhook route hit'); // Add this log
+        stripeController.createWebhook(req, res, next);
+      });
+      
 };
