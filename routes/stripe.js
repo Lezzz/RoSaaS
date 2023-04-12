bd9cfc2b5142fb8a90880059197bcc6acf8ac98c
Valentin Lazureanu <valentin.lazureanu@gmail.com>
2023-04-12 23:13:34 +0300

    fix stripe (ty mana <3 )

--- routes/stripe.js
+++ routes/stripe.js
@@ -3,5 +3,5 @@ const { protect } = require('../middleware/auth');
 
 module.exports = function(app){
     app.post('/checkout', protect, createCheckout);
-    app.post('/webhook', protect, createWebhook);
+    app.post('/webhook', createWebhook);
 }
